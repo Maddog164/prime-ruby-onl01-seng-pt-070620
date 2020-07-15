8d65a0c7dc9c2935929774c677b6737593459147
Denise Henderson <denise@henderson3.us>
2020-07-15 09:26:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+require 'pry'
+
+def prime?(num)
+  binding.pry
+  if num < 0
+    result = false
+  elsif num == 2
+    result = true
+  elsif num % 2 == 0
+    result = false
+  elsif num% 3 == 0
+    result = false
+  elsif num % 5 == 0
+    result = false
+  elsif num % 7 == 0
+    result = false
+  else
+    result = true
+  end
+  return result
+end
